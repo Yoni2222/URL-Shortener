0432b24243c1ad42cb321ffb5c1c5e08c3ec9bba
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2023-12-07 18:43:42 +0200

    second

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require('express');
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
@@ -55,7 +55,7 @@ app.post('/db', async (req, res) => {
 
     }
     catch(error){
-
+        console.log(error);
     }
 })
 
