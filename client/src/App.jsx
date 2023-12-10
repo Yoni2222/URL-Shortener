2853ce6f9050bf5e48579d3e75d22505be73cc5b
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2023-12-10 18:12:32 +0200

    one

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -181,7 +181,7 @@ const HeaderAndInput = (props) => {
 
 const ListOfURLs = (props) => {
   const space = " ";
-
+  //mongodb://localhost:27017/?retryWrites=true&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000
   return (
     
     <ul className = "list-group" style = {{width:"99.3%"}}>
