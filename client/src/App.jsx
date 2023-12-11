ce8c13215ecdf815ec66e7e50274319d81084fbc
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2023-12-11 17:50:41 +0200

    004

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -189,7 +189,7 @@ const ListOfURLs = (props) => {
         <li className = "list-group-item">
           <div className = "d-flex justify-content-between">
             <div className = "align-self-start">
-                <a href = {{HOST_URL} + `/newUrl/${elem.shortUrl}`}>{HOST_URL  + elem.shortUrl + " "} {space}</a>
+                <a href = {HOST_URL + `/newUrl/${elem.shortUrl}`}>{HOST_URL  + elem.shortUrl + " "} {space}</a>
             </div>
             <div className = "fs-5 ml-3" style = {{marginLeft: '600px'}}>
               
