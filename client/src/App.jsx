72d6423e46ae3057f9d7a2db98bc6801ac4592fc
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2023-12-14 17:30:35 +0200

    00..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -167,6 +167,8 @@ const ListOfURLs = (props) => {
   //`${HOST_URL}newUrl/${elem.shortUrl}`
 
   const handleClick = async (val, event) => {
+    event.preventDefault();
+    alert("im herrrre");
     try {
       const response = await fetch(HOST_URL + `newUrl/${val}`);
     }
@@ -183,7 +185,7 @@ const ListOfURLs = (props) => {
         <li className = "list-group-item">
           <div className = "d-flex justify-content-between">
             <div className = "align-self-start">
-                <a href = '#' onClick = {handleClick.bind(elem.shortUrl)}>{HOST_URL  + elem.shortUrl + " "} {space}</a>
+                <a href = '#' onClick = {handleClick.bind(null, elem.shortUrl)}>{HOST_URL  + elem.shortUrl + " "} {space}</a>
                 {/*<a href = {HOST_URL + `newUrl/${elem.shortUrl}`} onClick = {handleClick}>{HOST_URL  + elem.shortUrl + " "} {space}</a>*/}
                 {/*<a href = {HOST_URL + `newUrl/shortUrl`}>{HOST_URL  + elem.shortUrl + " "} {space}</a>*/}
             </div>
