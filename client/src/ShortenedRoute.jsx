8df4abb7f1d473480d80d8f62d49b8771bf1d608
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2023-12-16 20:08:17 +0200

    com4

--- client/src/ShortenedRoute.jsx
+++ client/src/ShortenedRoute.jsx
@@ -14,7 +14,7 @@ const ShortenedRoute = (props) => {
     const getFromDB = async() => {
         const path = props.match.params.shortUrl;
         try {
-        const response = await fetch(HOST_URL + "newUrl/shortUrl", {
+        const response = await fetch("/newUrl/shortUrl", {
             method: "POST",
             mode: "cors", 
             headers: {
