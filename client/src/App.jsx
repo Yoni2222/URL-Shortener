84aed334f9695cce1e0fe912239cd44b1e12b4e2
Yoni2222 <55746609+Yoni2222@users.noreply.github.com>
2024-08-26 23:53:26 +0300

    change a to 19 in feature-branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Main from './Main.jsx';
 import ShortenedRoute from './ShortenedRoute.jsx';
 
 const App = () => {
-  var a = 18;
+  var a = 19;
   return (
     <BrowserRouter>
       <Routes>
